6b5d6919ae86e3c2faae09154ee056576b5636f6
sabbana <sabana.a7@gmail.com>
2016-08-10 16:07:58 +0700

    update

--- application/config/config_site.php
+++ application/config/config_site.php
@@ -4,3 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config["google_api_key"] = 'AIzaSyAR-KRvOPGqBR8hUuBf9R4ER2YPXltpESs';
 $config["client_id"] = '113819832872-9vqrjr8l9ae941rbd6uco3ais1kvm77f.apps.googleusercontent.com';
 $config["client_secret"] = 'N4XSoikDW7Ovw-3IDvJDmTZs';
+
+# set credential youtube account
+$config['youtube_client_id'] = '140167196127-0ikd51nj1aci9thl5r64q7l8pceu17e8.apps.googleusercontent.com';
+$config['youtube_client_secret'] = '-6iEPQ4agUvwNfDgApQKqpNM';
